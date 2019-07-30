44c230a9e58421c68c1b92eed7c6ae053eaa84e2
herman <hermanpineda94@gmail.com>
2019-07-29 19:55:38 -0500

    Setup PM2

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [{
+    name: 'server',
+    script: './index.js'
+  }],
+  deploy: {
+    production: {
+      user: 'ubuntu',
+      host: 'ec2-13-58-209-69.us-east-2.compute.amazonaws.com',
+      key: '~/.ssh/hpineda_0.pem',
+      ref: 'origin/master',
+      repo: 'git@github.com:herman-19/Node.js-server-on-AWS',
+      path: '/home/ubuntu/server',
+      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+    }
+  }
+}
