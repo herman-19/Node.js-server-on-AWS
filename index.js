7ff738da85d7430a6744d01d7d0b31a7d1f7f9a2
herman <hermanpineda94@gmail.com>
2019-07-29 20:34:52 -0500

    Serve static file--express

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express')
-const app = express()
-app.get('/', (req, res) => {
-  res.send('HEY!')
-})
+const app     = express()
+
+// app.get('/', (req, res) => {
+//   res.send('HEY!')
+// })
+//app.listen(3000, () => console.log('Server running on port 3000'))
+
+app.use(express.static('public'))
 app.listen(3000, () => console.log('Server running on port 3000'))
