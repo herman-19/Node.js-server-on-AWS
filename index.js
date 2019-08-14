d2f0cee196568189966ec65ba45963c6f444cf0f
herman <hermanpineda94@gmail.com>
2019-08-13 23:10:06 -0500

    Store sensor data in text file

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-const express    = require('express')
-const app        = express()
-const bodyParser = require('body-parser')
+const express    = require('express');
+const app        = express();
+const bodyParser = require('body-parser');
+const fs         = require('fs');
 
 // The middleware below is used to parse the post data
-const urlEncodedParser = bodyParser.urlencoded({extended: false})
+const urlEncodedParser = bodyParser.urlencoded({extended: false});
 
 //app.get('/', (req, res) => {
 //   res.send('HEY!')
@@ -13,9 +14,10 @@ const urlEncodedParser = bodyParser.urlencoded({extended: false})
 // Post Handler--Note: urlEncodedParser will be kicked off everytime
 // we get a post request to the route /sensorData. It will parse data.
 app.post('/sensorData', urlEncodedParser, (req, res) => {
-  console.log(req.body) // will log the data posted to us.
-  res.send('RESPONSE!')
-})
+  console.log(req.body);
+  fs.appendFile('SensorData.txt', req.body, 'utf8');
+  res.send('Your data has been stored!');
+});
 
-app.use(express.static('public'))
-app.listen(3000, () => console.log('Server running on port 3000'))
+app.use(express.static('public'));
+app.listen(3000, () => console.log('Server running on port 3000'));
